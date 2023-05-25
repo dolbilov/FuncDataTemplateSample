78af3d6cab4f9f6a9726433ca5fad1d35e3ae87c
Kirill Dolbilov <dolbilov.kirill@gmail.com>
2023-05-25 13:52:43 +0300

    chore: create collection for people

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace FuncDataTemplateSample.ViewModels;
+﻿using System.Collections.Generic;
+using FuncDataTemplateSample.Models;
+
+namespace FuncDataTemplateSample.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public List<Person> People { get; } = new()
+    {
+        new Person
+        {
+            FirstName = "Mr.",
+            LastName = "X",
+            Sex = Sex.Diverse
+        },
+        new Person
+        {
+            FirstName = "Tony",
+            LastName = "Stark",
+            Sex = Sex.Male
+        },
+        new Person
+        {
+            FirstName = "Natasha",
+            LastName = "Romanoff",
+            Sex = Sex.Female
+        }
+    };
 }
